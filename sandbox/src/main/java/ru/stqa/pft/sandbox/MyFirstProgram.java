a507780471dac172aa219240fe1a331a384cc3c1
Deryagin Sergey <sergey.deryagin@infotecs.ru>
2018-11-23 20:05:36 +0300

    Lesson 1, task 2 result.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,18 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+        // First part of second task
+        Point point1 = new Point(1, 1);
+        Point point2 = new Point(5, 5);
+        System.out.println(distance(point1, point2));
+
+        // second part of second task
+        System.out.println(point1.distance(5, 5));
+
 	}
+
+    public static double distance(Point first, Point second) {
+
+	    return Math.sqrt( ((second.x - first.x) * (second.x - first.x)) + ((second.y - first.y) * (second.y - first.y)) );
+    }
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+    public double x;
+    public double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(double x, double y) {
+        return Math.sqrt( ((x - this.x) * (x - this.x)) + ((y - this.y) * (y - this.y)) );
+    }
+
+
+}
