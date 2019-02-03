c462eddb693c20c2aa3da46e0ab1e105aadc6975
sergeyderyagin <sergeyderyagin87@gmail.com>
2019-02-03 13:27:08 +0300

    Add new module for mantis tests.

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -0,0 +1,26 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+
+import java.io.IOException;
+
+
+public class TestBase {
+
+    private static ApplicationManager app =
+            new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+
+    @BeforeSuite(alwaysRun = true)
+    public void setUp() throws IOException {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() {
+        app.stop();
+    }
+
+}
